0e45b9c532860c9d04797328143f15a9c5d06320
Prashant Choudhary <prash.sastra1@gmail.com>
2024-08-18 21:50:00 +0530

    Java Practice Programs - Day 3

---
+++ src/arrays/TwoDimArrays.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class TwoDimArrays {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2,3},
+                {4,5},
+                {6,7,8,9}
+        };
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+
+        for(int row = 0; row < arr.length; row++) {
+            for(int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
